b263d029a96d907787d0802ac31cf530781f3eff
Lovmong <1967471283@qq.com>
2023-03-15 16:58:48 +0800

    update firmware

--- burn esp8266 with pico/install.bat
+++ burn esp8266 with pico/install.bat
@@ -1,12 +1,13 @@
 @REM esptool.exe -help
 
-esptool.exe --chip esp8266 --before no_reset_no_sync erase_flash
+@REM set firmware=%1
+set firmware=esp8266-uart-wsserver-v2.0.0.bin
 
-esptool.exe --chip esp8266 --before no_reset_no_sync write_flash 0 "esp8266-uart-wsserver-v1.1.0.bin"
+esptool.exe --chip esp8266 --before no_reset_no_sync erase_flash
+esptool.exe --chip esp8266 --before no_reset_no_sync write_flash 0 %firmware%
 
 @REM esptool.exe --chip esp8266 --before default_reset erase_flash
-
-@REM esptool.exe --chip esp8266 --before default_reset write_flash 0 "esp8266-uart-wsserver-v1.1.0.bin"
+@REM esptool.exe --chip esp8266 --before default_reset write_flash 0 "%firmware%"
 
 pause
 
